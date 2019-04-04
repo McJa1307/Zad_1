d5c894856ddd31c307305b807cd6b8e9e7b69945
Amakazor <amakazor@gmail.com>
2019-04-05 00:50:45 +0200

    dupa cipa

--- Zad_2/Zad_2/Zad_2.cpp
+++ Zad_2/Zad_2/Zad_2.cpp
@@ -9,28 +9,39 @@
 #include <sstream>
 #include <math.h>
 
+using namespace std;
+
+class Convert_from_symbolic
+{
 #define TRUE true
 #define FALSE false
 //bo du¿ymi literami jest ³adniej
 
-using namespace std;
+public:
+	Convert_from_symbolic(string);
+	~Convert_from_symbolic();
+	string Convert(string);
+	string Convert();
 
-double ipow(double base, int exp)
+private:
+	string input;
+
+	double convert_symbol_to_zeros(string);
+	int get_dot_pos(string);
+	int get_fraction_part_len(string, int);
+};
+
+Convert_from_symbolic::Convert_from_symbolic(string _input)
 {
-	double result = 1ULL;
-	while (exp)
-	{
-		if (exp & 1)
-		{
-			result *= (double)base;
-		}
-		exp >>= 1;
-		base *= base;
-	}
-	return result;
+	input = _input;
 }
 
-double convert_symbol_to_zeros(string symbol)
+Convert_from_symbolic::~Convert_from_symbolic()
+{
+}
+
+
+double Convert_from_symbolic::convert_symbol_to_zeros(string symbol)
 {
 	if (symbol == "M")		return  6;
 	else if (symbol == "B")		return  9;
@@ -43,19 +54,19 @@ double convert_symbol_to_zeros(string symbol)
 	else						return  0;
 }
 
-int get_dot_pos(string input)
+int Convert_from_symbolic::get_dot_pos(string input)
 {
 	size_t index = input.find('.');
 	return static_cast<int>(index);
 }
 
-int get_fraction_part_len(string input, int dot_pos)
+int Convert_from_symbolic::get_fraction_part_len(string input, int dot_pos)
 {
 	if (dot_pos == -1) return -1;
 	return (static_cast<int>(input.length()) - (dot_pos + 1));
 }
 
-string ConvertFromSymbolic(string str)
+string Convert_from_symbolic::Convert(string str)
 {
 	string number_part;
 	string symbol_part;
@@ -123,18 +134,98 @@ string ConvertFromSymbolic(string str)
 	return output;
 }
 
+string Convert_from_symbolic::Convert()
+{
+	if (input.length() > 0)
+	{
+		string str = input;
+
+		string number_part;
+		string symbol_part;
+
+		bool more_digits = TRUE;
+
+		for (int i = 0; i < str.length(); i++)
+		{
+
+			if (isdigit(str[i]) && more_digits)
+			{
+				number_part += str[i];
+			}
+			else
+			{
+				if (i != 0)
+				{
+					if (str[i] == '.' && more_digits)
+					{
+						number_part += str[i];
+					}
+					else
+					{
+						symbol_part += str[i];
+						more_digits = FALSE;
+					}
+
+				}
+			}
+		}
+
+		int zeros = convert_symbol_to_zeros(symbol_part);
+		int dot_pos = get_dot_pos(number_part);
+		int fraction_part_len = get_fraction_part_len(number_part, dot_pos);
+
+		string output = "";
+
+		if (fraction_part_len > 0)
+		{
+			if (zeros >= fraction_part_len)
+			{
+				output = number_part.erase(dot_pos, 1);
+				zeros -= fraction_part_len;
+			}
+			else
+			{
+				output = number_part.erase(dot_pos, 1).insert(dot_pos + zeros, ".");
+				zeros = 0;
+			}
+		}
+		else if (fraction_part_len == 0)
+		{
+			output = number_part.erase(dot_pos, 1);
+		}
+		else
+		{
+			output = number_part;
+		}
+
+		for (int i = zeros; i > 0; i--)
+		{
+			output += "0";
+		}
+
+		return output;
+	}
+	else
+	{
+		return "Niepodano ci¹gu znaków w konstruktorze";
+	}
+}
+
 int main()
 {
-	cout << (ConvertFromSymbolic("4.2222222M")) << endl;
-	cout << (ConvertFromSymbolic("4.222222222222M")) << endl;
-	cout << (ConvertFromSymbolic("4.222222M")) << endl;
-	cout << (ConvertFromSymbolic("4.222M")) << endl;
-	cout << (ConvertFromSymbolic("4M")) << endl;
-	cout << (ConvertFromSymbolic("54T")) << endl;
-	cout << (ConvertFromSymbolic("5.434B")) << endl;
-	cout << (ConvertFromSymbolic("1Oc")) << endl;
-	cout << (ConvertFromSymbolic("84340000Oc")) << endl;
-	cout << (ConvertFromSymbolic("943400000Oc")) << endl;
+	Convert_from_symbolic cfs1("48B");
+	cout << cfs1.Convert() << endl;
+	Convert_from_symbolic cfs2("42.78Oc");
+	cout << cfs2.Convert() << endl;
+	Convert_from_symbolic cfs3("788.42Qa");
+	cout << cfs3.Convert() << endl;
+
+	Convert_from_symbolic cfs21();
+	cout << cfs3.Convert("99Qi") << endl;
+	Convert_from_symbolic cfs22();
+	cout << cfs3.Convert("21.9990999999999999999T") << endl;
+	Convert_from_symbolic cfs23();
+	cout << cfs3.Convert("5.65M") << endl;
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
